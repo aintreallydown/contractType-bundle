82eb0d93e758b8e364f8c4c281e4f51fea99c7a5
92radar <viepro.db@gmail.com>
2026-08-18 15:18:27 +0200

    fix: Change form field type from TextType to ChoiceType in ContractTypeFormType

--- src/Form/ContractTypeFormType.php
+++ src/Form/ContractTypeFormType.php
@@ -3,7 +3,7 @@
 namespace aintreallydown\ContractTypeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\Tenant;
@@ -13,7 +13,7 @@ class ContractTypeFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contract', TextType::class, [
+            ->add('contract', ChoiceType::class, [
                 'mapped' => false,
             ]);
     }
