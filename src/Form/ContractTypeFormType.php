8fec79d82dc7495bfdbe5f2dc42742ead5d20a0a
92radar <viepro.db@gmail.com>
2026-08-18 14:55:42 +0200

    fix: Correct form field name from 'contractType' to 'contract' in ContractTypeFormType

--- src/Form/ContractTypeFormType.php
+++ src/Form/ContractTypeFormType.php
@@ -13,7 +13,7 @@ class ContractTypeFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('contractType', TextType::class, [
+            ->add('contract', TextType::class, [
                 'mapped' => false,
             ]);
     }
